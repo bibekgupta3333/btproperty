7ed63a3981f07e90f1e2885808fd064ff20ee7df
Priyanka Gupta <pggnit213@gmail.com>
2019-07-26 21:20:13 +0545

    first commit

---
+++ contacts/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+from .models import Contact
+
+class ConatactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'listing', 'email',
+                    'contact_date')
+    list_display_links = ('id', 'name')
+    search_fields = ('name', 'email', 'listing')
+    list_per_page = 25
+
+admin.site.register(Contact, ConatactAdmin)
